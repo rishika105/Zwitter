d19065354cf28be655d38efee4b1955ade3a0b46
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2025-09-25 19:37:34 +0530

    graphgql server setup

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express from "express"
+
+import dotenv from "dotenv";
+dotenv.config();
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@as-integrations/express5";
+
+async function init() {
+    const app = express();
+
+    const PORT = Number(process.env.PORT) || 7000;
+
+    app.use(express.json())
+
+    //create graphql server
+    const gqlServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                hello: String
+                say(name: String): String
+            }
+        `, //Schema
+        resolvers: {
+            Query: {
+                hello: () => `hey there i am graphql resolver func`,
+                say: (_, {name}: {name: string} )=> `Hey ${name} How are you?`
+            }
+        }  //Function
+    })
+
+    await gqlServer.start(); //to use await we need to make a function cant run locally
+
+
+    app.get('/', (req, res) => {
+        res.json({message: "Server is up and running"})
+    });
+
+    app.use("/graphql", expressMiddleware(gqlServer))
+
+    app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`))
+}
+
+init();
